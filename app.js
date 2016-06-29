79b5e62905ea7cc9969eb71dc847eefc46910e91
Sukesh chand <sukeshchand@hotmail.com>
2016-06-29 15:52:29 +0530

    Added html text inside nodes.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
 //
 // Define the 'app' module.
-//
-angular.module('app', ['flowChart', ])
+//angular.module('mySceApp', ['ngSanitize'])
+angular.module('app', ['flowChart', 'ngSanitize']).
+    filter('html', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}])
 
 //
 // Simple service to create a prompt.
@@ -58,105 +63,128 @@ angular.module('app', ['flowChart', ])
 	// Setup the data-model for the chart.
 	//
 	var chartDataModel = {
-        nodes: [
-			{
-				name: "Example Node 1",
-				id: 0,
-				x: 0,
-				y: 0,
-				width: 600,
-				height: 200,
-				NodeDirection: "Vertical",
-				inputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-				outputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-			},
-
-			{
-				name: "Example Node 2",
-				id: 1,
-				x: 400,
-				y: 200,
-				width: 600,
-				height: 200,
-				NodeDirection: "Vertical",
-				inputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-				outputConnectors: [
-					{
-						name: "A",
-					},
-					{
-						name: "B",
-					},
-					{
-						name: "C",
-					},
-				],
-			},
-
-		],
-
-		connections: [
-			{
-				name:'Connection 1',
-				source: {
-					nodeID: 0,
-					connectorIndex: 1,
-				},
-
-				dest: {
-					nodeID: 1,
-					connectorIndex: 2,
-				},
-			},
-			{
-				name:'Connection 2',
-				source: {
-					nodeID: 0,
-					connectorIndex: 0,
-				},
-
-				dest: {
-					nodeID: 1,
-					connectorIndex: 0,
-				},
-			},
-
-		]
+	    "nodes": [
+            {
+                "name": "Deposit Criteria - <b>200 SEK</b>",
+                "id": 0,
+                "x": 391,
+                "y": 117,
+                "width": 200,
+                "height": 80,
+                "NodeDirection": "Vertical",
+                "outputConnectors": [
+                    {
+                        "name": "A"
+                    },
+                    {
+                        "name": "New connector"
+                    }
+                ]
+            },
+            {
+                "name": "Netent Bonus<br/> Bonus Code: <b>HHDD003</B>",
+                "id": 1,
+                "x": 586,
+                "y": 315,
+                "width": 220,
+                "height": 99,
+                "NodeDirection": "Vertical",
+                "inputConnectors": [
+                    {
+                        "name": "A"
+                    }
+                ],
+                "outputConnectors": [
+                    {
+                        "name": "A"
+                    }
+                ]
+            },
+            {
+                "name": "Turn Over Criteria<br/> Min Turn Over: <b>200 SEK</b>",
+                "id": 10,
+                "x": 143,
+                "y": 303,
+                "width": 200,
+                "height": 99,
+                "NodeDirection": "Vertical",
+                "inputConnectors": [
+                    {
+                        "name": "X"
+                    }
+                ],
+                "outputConnectors": [
+                    {
+                        "name": "1"
+                    }
+                ]
+            },
+            {
+                "name": "Call Center Criteria",
+                "id": 11,
+                "x": 380,
+                "y": 592,
+                "width": 200,
+                "height": 80,
+                "NodeDirection": "Vertical",
+                "inputConnectors": [
+                    {
+                        "name": "X"
+                    },
+                    {
+                        "name": "X"
+                    }
+                ]
+            }
+	    ],
+	    "connections": [
+            {
+                "source": {
+                    "nodeID": 0,
+                    "connectorIndex": 0
+                },
+                "dest": {
+                    "nodeID": 1,
+                    "connectorIndex": 0
+                }
+            },
+            {
+                "source": {
+                    "nodeID": 0,
+                    "connectorIndex": 1
+                },
+                "dest": {
+                    "nodeID": 10,
+                    "connectorIndex": 0
+                }
+            },
+            {
+                "source": {
+                    "nodeID": 10,
+                    "connectorIndex": 0
+                },
+                "dest": {
+                    "nodeID": 11,
+                    "connectorIndex": 1
+                }
+            },
+            {
+                "source": {
+                    "nodeID": 1,
+                    "connectorIndex": 0
+                },
+                "dest": {
+                    "nodeID": 11,
+                    "connectorIndex": 0
+                }
+            }
+	    ]
 	};
 
 	//
 	// Event handler for key-down on the flowchart.
-	//
+    //
+    
 	$scope.keyDown = function (evt) {
 
 		if (evt.keyCode === ctrlKeyCode) {
@@ -217,26 +245,17 @@ angular.module('app', ['flowChart', ])
 			id: nextNodeID++,
 			x: 0,
 			y: 0,
+			width: 600,
+			height: 200,
+			NodeDirection: "Vertical",
 			inputConnectors: [
 				{
 					name: "X"
-				},
-				{
-					name: "Y"
-				},
-				{
-					name: "Z"
 				}
 			],
 			outputConnectors: [ 
 				{
 					name: "1"
-				},
-				{
-					name: "2"
-				},
-				{
-					name: "3"
 				}
 			],
 		};
